7f786f0e193d0ab5d285a2cd2add15569fe39feb
George Louthan <georgerlouth@nthefourth.com>
2014-07-01 02:11:14 -0500

    Working IR test for the shield. Shield looks to be confirmed badge-compatible.

--- qcxi.h
+++ qcxi.h
@@ -9,7 +9,7 @@
 #define MAIN_H_
 
 // Configuration flags (TODO: should probably be in a make file):
-#define BADGE_TARGET 1
+#define BADGE_TARGET 0
 #define DEBUG_SERIAL 0
 
 #define SYNC0 0b11001100
@@ -30,12 +30,15 @@
 // IR:
 #define IR_USCI_BASE USCI_A1_BASE
 #define IR_USCI_VECTOR USCI_A1_VECTOR
-#define IR_TXRX_PORT 4
-#define IR_SD_PORT 4
-#define IR_TX_PIN 4
-#define IR_RX_PIN 5
-#define IR_SD_PIN 6
+#define IR_TXRX_PORT GPIO_PORT_P4
+#define IR_SD_PORT GPIO_PORT_P4
+#define IR_TX_PIN GPIO_PIN4
+#define IR_RX_PIN GPIO_PIN5
+#define IR_SD_PIN GPIO_PIN6
 #define IRIV UCA1IV
+
+#define IRTCTL UCA1IRTCTL
+#define IRRCTL UCA1IRRCTL
 
 // Radio:
 #define RFM_NSS_PORT GPIO_PORT_P4
@@ -45,19 +48,21 @@
 #define UCS_XT2_CRYSTAL_FREQUENCY 16000000
 
 #else
-#include <msp430f5529.h>
 // Target is the Launchpad+shield:
-#include <msp430f5308.h>
+#include <msp430f5329.h>
 
 // IR:
 #define IR_USCI_BASE USCI_A0_BASE
 #define IR_USCI_VECTOR USCI_A0_VECTOR
 #define IRIV UCA0IV
-#define IR_TXRX_PORT 3
-#define IR_SD_PORT 8
-#define IR_TX_PIN 3
-#define IR_RX_PIN 4
-#define IR_SD_PIN 1
+#define IR_TXRX_PORT GPIO_PORT_P3
+#define IR_SD_PORT GPIO_PORT_P8
+#define IR_TX_PIN GPIO_PIN3
+#define IR_RX_PIN GPIO_PIN4
+#define IR_SD_PIN GPIO_PIN1
+
+#define IRTCTL UCA0IRTCTL
+#define IRRCTL UCA0IRRCTL
 
 // Radio:
 #define RFM_NSS_PORT GPIO_PORT_P3
