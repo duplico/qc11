a32819f7868dff89af11b6e362b7c5f9526e9bcb
George Louthan <georgerlouth@nthefourth.com>
2014-07-01 22:48:40 -0500

    Add a Python script to generate arrays RGB values to make rainbows.

---
+++ etc/rainbow_generator.py
@@ -0,0 +1,9 @@
+import math
+
+frequency = 0.3
+
+for i in range(21):
+    red = math.sin(frequency*i + 0) * 127+128
+    green = math.sin(frequency*i + 2) * 127+128
+    blue = math.sin(frequency*i+4) * 127+128
+    print "{ 0x%x, 0x%x, 0x%x }," % (red/10, green/10, blue/10)
