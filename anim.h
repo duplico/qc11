13d99d1d97a466f1d43d9795eece0fc8b46bd299
George Louthan <191906+duplico@users.noreply.github.com>
2014-07-21 14:26:51 -0500

    Break out animations into a separate file.

---
+++ anim.h
@@ -0,0 +1,17 @@
+/*
+ * anim.h
+ *
+ *  Created on: Jul 21, 2014
+ *      Author: glouthan
+ */
+
+#ifndef ANIM_H_
+#define ANIM_H_
+#include "leds.h"
+
+extern const spriteframe anim_sprite_wave[];
+extern const spriteframe anim_sprite_walkin[];
+extern const spriteframe * tricks[];
+extern const spriteframe stand[];
+
+#endif /* ANIM_H_ */
